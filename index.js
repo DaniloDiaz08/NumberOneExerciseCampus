891ab09a3000bf667ff4a5a07fab4778c15f33e2
danilodiaz <diazdanilo340@gmail.com>
2023-03-10 17:33:40 -0500

    Complete

---
+++ index.js
@@ -0,0 +1,4 @@
+/* Para esta ocasión se debe configurar un script en JSON, 
+que nos debe presentar un archivo Javascript en panatalla */
+
+console.log("Hola mundo, este es mi primer ejercicio con Node.js en el mejor Bootcamp de programación del mundo.")
